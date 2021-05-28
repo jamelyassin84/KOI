d9d1beec8eb3ca1c4265b24ea55c6d335774c861
Jamel Yassin <jamelyassin84@gmail.com>
2021-05-28 18:41:36 +0800

    changes

--- Web/src/app/app.module.ts
+++ Web/src/app/app.module.ts
@@ -1,38 +1,32 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing';
+import { NavbarModule } from './shared/navbar/navbar.module';
+import { FooterModule } from './shared/footer/footer.module';
+import { SidebarModule } from './sidebar/sidebar.module';
+
 import { AppComponent } from './app.component';
-import { LoginComponent } from './Pages/login/login.component';
-import { KoisComponent } from './Pages/home/kois/kois.component';
-import { KoisAddComponent } from './Pages/home/add/kois-add/kois-add.component';
-import { FoodAddComponent } from './Pages/home/add/food-add/food-add.component';
-import { DiseaseAddComponent } from './Pages/home/add/disease-add/disease-add.component';
-import { ContainerAddComponent } from './Pages/home/add/container-add/container-add.component';
-import { KoisViewComponent } from './Pages/home/views/kois-view/kois-view.component';
-import { KoiViewComponent } from './Pages/home/views/koi-view/koi-view.component';
-import { FoodViewComponent } from './Pages/home/views/food-view/food-view.component';
-import { DiseaseViewComponent } from './Pages/home/views/disease-view/disease-view.component';
-import { ContainerViewComponent } from './Pages/home/views/container-view/container-view.component'
+
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
 @NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    RouterModule,
+    HttpClientModule,
+    NavbarModule,
+    FooterModule,
+    SidebarModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
-    LoginComponent,
-    KoisComponent,
-    KoisAddComponent,
-    FoodAddComponent,
-    DiseaseAddComponent,
-    ContainerAddComponent,
-    KoisViewComponent,
-    KoiViewComponent,
-    FoodViewComponent,
-    DiseaseViewComponent,
-    ContainerViewComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    AdminLayoutComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
